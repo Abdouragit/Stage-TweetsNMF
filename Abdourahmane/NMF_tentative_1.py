6e7302f1289a2e6f46e12d019eb050830111554f
Abdouragit <105711497+Abdouragit@users.noreply.github.com>
2022-06-14 12:02:26 +0200

    MAJ
    mise à jour word + avancement code python

--- Abdourahmane/NMF_tentative_1.py
+++ Abdourahmane/NMF_tentative_1.py
@@ -4,7 +4,7 @@
 
 import pandas as pd #on en aura besoin pour manipuler des dataframes mais aussi pour importer/exporter des données
 
-from sklearn.feature_extraction import TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.decomposition import NMF
 from sklearn.feature_extraction import text
 # sklearn est surement une bibliothèque remplie de modules et chaque module est remplie de classes qu'on importe
@@ -27,9 +27,66 @@ import string
 
 pd.set_option('max_colwidth',150)
 
-df = pd.read_csv('inaug_speeches.csv',engine = 'python')
+df = pd.read_csv('Abdourahmane\inaug_speeches.csv',engine = 'python')
 
-df.head()
+print()
+print(df.head()) # afin de voir les première lignes de donnée
+
+# ---- Isoler les données
+
+"""I’m going to make a dataframe of the President’s names and 
+speeches, and isolate to the first term inauguration speech, 
+because some President’s did not have a second term. This makes 
+for a corpus of documents with similar lengths."""
+
+df = df.drop_duplicates(subset=['Name'],keep='first')
+
+df = df.reset_index() #je ne sais pas ce qu'est l'index
+
+df = df[['Name','text']]
+
+df = df.set_index('Name')
+
+print()
+print (df.head())
+
+
+# ---- Nettoyage des données
+
+"""I want to make all of the text in the speeches as comparable 
+as possible so I will create a cleaning function that removes 
+punctuation, capitalization, numbers, and strange characters. 
+
+I use regular expressions for this, which offers a lot of ways 
+to ‘substitute’ text. 
+
+There are tons of regular expression cheat sheets, like this one. 
+I ‘apply’ this function to my speech column"""
+
+def clean_text_round1(text): # je ne sais pas vraiment ce qu'on fait dans cette fonction
+    '''Make text loweracse, remove text in square brackets, 
+    remove punctuation, remove read errors, and remove words
+    containing numbers.'''
+
+    text = text.lower()
+    text = re.sub('\[.*?\]',' ',text)
+    text = re.sub('[%s]' % re.escape(string.punctuation),' ',text)
+    text = re.sub('\w*\d\w*',' ',text)
+    text = re.sub('�',' ', text)
+
+    return text
+
+round1 = lambda x: clean_text_round1(x)
+
+# Clean Speech Text
+df["text"] = df["text"].apply(round1)
+
+print()
+print(df.head())
+
+
+# ---- Creer une fonction qui va préparer les données une premiere fois
+# ou un truc du genre
 
 
 
